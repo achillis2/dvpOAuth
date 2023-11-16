929956a174bfaf2d26fb23cc008d20443fd22ed8
DejunJacksonRI <98330763+DejunJacksonRI@users.noreply.github.com>
2023-11-16 00:26:03 -0600

    Add code for Usage API (#1)
    * add Usage API
    
    * Add conversion function for usage
    
    * add headers to clear encoding error

--- dvpOAuth/class_OAuth.py
+++ dvpOAuth/class_OAuth.py
@@ -2,12 +2,12 @@ import requests
 import json
 import os
 import logging
-import re
+import re, pdb
 
 from functools import wraps
 from .error import AuthClientError, MissingTokenError
 
-from .util import SEARCH_PAR, RETRIEVE_PAR
+from .util import SEARCH_PAR, RETRIEVE_PAR, USAGE_PAR
 from collections import defaultdict
 
 from datetime import datetime
@@ -459,3 +459,101 @@ class DVPOAuth:
         return resp_query.status_code, resp
 
 
+    @_renew_token
+    def account_usage(self, *args, **kwargs):
+        """retrieve account usage info based on the account number"""
+        USAGE_BASE_URL = os.environ["USAGE_BASE_URL"]
+
+        if not self.token:
+            raise MissingTokenError
+
+        auth = "Bearer " + self.token
+
+        # Usage API
+
+        dict = defaultdict(str, args[0])
+        try:
+            if dict["account"]:
+                account = dict["account"] if dict["account"] else None
+                start_date = dict["start_date"] if dict["start_date"] else None
+                end_date = dict["end_date"] if dict["end_date"] else None
+                filter = (
+                    USAGE_PAR
+                    + account
+                    + "' and Action eq '5' and DateRange/StartDate eq datetime'"
+                    + start_date
+                    + "' and DateRange/EndDate eq datetime'"
+                    + end_date
+                    + "'"
+                )
+            else:
+                raise ValueError
+        except:
+            pass
+            """handle exception here"""
+        usage_params = {
+            "$format": "json",
+            "$filter": filter,
+            "$expand": "ZUsageStatement",
+        }
+        headers={
+            'Accept-Encoding':'gzip,deflate',
+            "Authorization": auth
+        }
+        resp_query = requests.get(
+            USAGE_BASE_URL, params=usage_params, headers=headers
+        )
+
+        if self.logging:
+            logger.info(f"Usage status code : {resp_query.status_code}")
+
+        if resp_query.status_code == 401:
+            if self.logging:
+                logger.exception("Account usage error.")
+            raise AuthClientError
+
+        resp = json.loads(resp_query.content)
+        if self.logging:
+            logger.info(f"Usage response : {resp}")
+        return resp_query.status_code, resp
+
+    def convert_account_usage_json_from_oauth_to_soap(
+        self, status_code=None, resp=None
+    ):
+        """convert the usage result json to the soap format"""
+        if not resp["d"]["results"]:
+            return None
+
+        resp_parent = resp["d"]["results"][0]
+        list_data = []
+        for data in resp_parent['ZUsageStatement']['results']:
+            MRDate = data['MRDate']
+            list_data.append(
+                {
+                    "AccountNumber": data["ContractAccount"],
+                    "RateCode": data["RateCode"],
+                    "MeterReadDate": datetime.fromtimestamp(eval(re.findall(r"\((.*?)\)", MRDate)[0])/ 1000).strftime("%Y-%m-%d-%H.%M.%S.%f"),  # ??? need parse the timestamp /Date(1672174552000)/
+                    "RateCodeDescription": data["RateCodeDescription"],
+                    "BillingDays": data["BillingDays"],
+                    "TotalBilledAmount": data["TotalBilledAmount"],
+                    "MeterStatus": data["MeterStatus"],
+                    "MRMethod": data["MRMethod"],
+                    "MeterRead": data["MeterRead"],
+                    "Usage": data["Usage"],
+                    "Demand": data["Demand"],
+                    "AvgDailyUse": data["AvgDailyUse"],
+                    "Meter": data["MeterID"],
+                }
+            )
+        
+        return_dict = {
+            "AccountUsageSequence": {
+                "AccountUsageSequence": list_data
+            },
+            'Header': {
+                'DateTimeStamp': datetime.now().strftime("%Y-%m-%d-%H.%M.%S.%f"),
+                'MessageText': resp_parent['ReturnMessage'],
+                'ReturnCode': resp_parent['ResultCode']
+            },
+        }
+        return return_dict
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-VERSION = '0.0.20'
+VERSION = '0.0.21'
 DESCRIPTION = 'Dominion API python wrapper'
 
 setup(
